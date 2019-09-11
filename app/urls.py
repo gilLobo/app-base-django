2c6e000937071b7305fb58945b19d5aca989a212
Gilberto Lobo <gilberto.lobo@servicenet.com.br>
2019-09-11 13:45:42 -0300

    Adicionando edicao e remocao

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('listar_tarefas/', listar_tarefas, name='listar_tarefas'),
-    path('cadastrar_tarefas/', cadastrar_tarefas, name='cadastrar_tarefas')
+    path('cadastrar_tarefas/', cadastrar_tarefas, name='cadastrar_tarefas'),
+    path('editar_tarefa/<int:id>', editar_tarefa, name='editar_tarefa'),
+    path('remover_tarefa/<int:id>', remover_tarefa, name='remover_tarefa')
+
 ]
