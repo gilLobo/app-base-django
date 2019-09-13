b6464a68cdfaaeb77787c6e82fc5b255f1a4caf7
Gilberto Lobo <gilberto.lobo@servicenet.com.br>
2019-09-13 09:35:59 -0300

    Autenticacao usuario

--- app/entidades/tarefa.py
+++ app/entidades/tarefa.py
@@ -1,14 +1,15 @@
 class Tarefa():
-    def __init__(self, titulo, descricao, data_expiracao, prioridade):
+    def __init__(self, titulo, descricao, data_expiracao, prioridade, usuario):
         self.__titulo = titulo
         self.__descricao = descricao
         self.__data_expiracao = data_expiracao
         self.__prioridade = prioridade
+        self.__usuario = usuario
 
     @property
     def titulo(self):
         return self.__titulo
-    
+
     @titulo.setter
     def titulo(self, titulo):
         self.__titulo = titulo
@@ -16,7 +17,7 @@ class Tarefa():
     @property
     def descricao(self):
         return self.__descricao
-    
+
     @descricao.setter
     def descricao(self, descricao):
         self.__descricao = descricao
@@ -24,7 +25,7 @@ class Tarefa():
     @property
     def data_expiracao(self):
         return self.__data_expiracao
-    
+
     @data_expiracao.setter
     def data_expiracao(self, data_expiracao):
         self.__data_expiracao = data_expiracao
@@ -32,9 +33,15 @@ class Tarefa():
     @property
     def prioridade(self):
         return self.__prioridade
-    
+
     @prioridade.setter
     def prioridade(self, prioridade):
         self.__prioridade = prioridade
 
-    
+    @property
+    def usuario(self):
+        return self.__usuario
+
+    @usuario.setter
+    def usuario(self, usuario):
+        self.__usuario = usuario
